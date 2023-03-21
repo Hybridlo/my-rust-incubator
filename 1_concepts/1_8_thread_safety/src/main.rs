003795fd281fee51df273f763947ff452125049d
Павел <pashanoy@gmail.com>
2023-03-21 10:24:37 +0200

    finish task 1_8

--- 1_concepts/1_8_thread_safety/src/main.rs
+++ 1_concepts/1_8_thread_safety/src/main.rs
@@ -1,3 +1,74 @@
+use std::{thread, rc::Rc};
+
+mod thread_structs;
+
+use thread_structs::{SyncAndSend, NotSyncNorSend, OnlySend, OnlySync};
+
 fn main() {
-    println!("Implement me!");
+    let only_sync = OnlySync::new(1);
+
+    // we can borrow OnlySync immutably, since it's Sync
+    thread::scope(|s| {
+        s.spawn(|| {
+            println!("{}", only_sync.get_value());
+        });
+    });
+
+    // OnlySync can't be sent to another thread since it's not Send (compile time error)
+    /* thread::scope(|s| {
+        s.spawn(move || {
+            only_sync.change_value(2);
+        });
+    }); */
+
+
+    let only_send = OnlySend::default();
+
+    // we can't borrow OnlySend from another thread (compile time error)
+    /* thread::scope(|s| {
+        s.spawn(|| {
+            println!("{}", *(only_send.value.borrow()));
+        });
+    }); */
+
+    // but we can move it to another thread
+    thread::scope(|s| {
+        s.spawn(move || {
+            *(only_send.value.borrow_mut()) = 2;
+        });
+    });
+
+
+    let mut sync_and_send = SyncAndSend::default();
+
+    // we can borrow SyncAndSend
+    thread::scope(|s| {
+        s.spawn(|| {
+            println!("{}", sync_and_send.value);
+        });
+    });
+
+    // and also can move it to another thread
+    thread::scope(|s| {
+        s.spawn(move || {
+            sync_and_send.value = 2;
+        });
+    });
+
+
+    let mut not_sync_nor_send = NotSyncNorSend::default();
+
+    // we can't borrow NotSyncNorSend (compile time error)
+    /* thread::scope(|s| {
+        s.spawn(|| {
+            println!("{}", *not_sync_nor_send.value);
+        });
+    }); */
+
+    // nor can we move it to another thread (compile time error)
+    /* thread::scope(|s| {
+        s.spawn(move || {
+            Rc::get_mut(&mut not_sync_nor_send.value);
+        });
+    }); */
 }
