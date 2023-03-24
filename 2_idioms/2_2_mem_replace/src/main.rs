59ce76f6c3e9f2e5ab50386dc371f45e474a3743
Павел <pashanoy@gmail.com>
2023-03-24 11:36:31 +0200

    finish task2_2

--- 2_idioms/2_2_mem_replace/src/main.rs
+++ 2_idioms/2_2_mem_replace/src/main.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 fn main() {
     let mut s = Solver {
         expected: Trinity { a: 1, b: 2, c: 3 },
@@ -9,7 +11,20 @@ fn main() {
         ],
     };
     s.resolve();
-    println!("{:?}", s)
+    println!("{:?}", s);
+
+
+    let mut s = Solver {
+        expected: Trinity { a: 1, b: 2, c: 3 },
+        unsolved: vec![
+            Trinity { a: 1, b: 2, c: 3 },
+            Trinity { a: 2, b: 1, c: 3 },
+            Trinity { a: 2, b: 3, c: 1 },
+            Trinity { a: 3, b: 1, c: 2 },
+        ],
+    };
+    s.resolve_noclone();
+    println!("noclone version: {:?}", s);
 }
 
 #[derive(Clone, Debug, PartialEq)]
@@ -19,14 +34,25 @@ struct Trinity<T> {
     c: T,
 }
 
-impl<T: Clone> Trinity<T> {
+impl<T> Trinity<T> {
     fn rotate(&mut self) {
-        let a = self.a.clone();
+        // self.b = self.a, self.a = self.b
+        mem::swap(&mut self.a, &mut self.b);
+        // self.c = self.b (has the initial value of self.a), self.b = self.c
+        mem::swap(&mut self.b, &mut self.c);
+        // result (right hand side is old/initial values):
+        // self.a = self.b
+        // self.b = self.c
+        // self.c = self.a
+
+
+
+        /* let a = self.a.clone();
         let b = self.b.clone();
         let c = self.c.clone();
         self.a = b;
         self.b = c;
-        self.c = a;
+        self.c = a; */
     }
 }
 
@@ -37,8 +63,30 @@ struct Solver<T> {
 }
 
 impl<T: Clone + PartialEq> Solver<T> {
+    /// optional: if we MUST keep self.expected unchanged;
+    /// a noclone version is provided in resolve_noclone
     fn resolve(&mut self) {
-        let mut unsolved = Vec::with_capacity(self.unsolved.len());
+        let mut expected = self.expected.clone();
+
+        self
+            .unsolved
+            .retain(|unsolved| {
+                for  _ in 0..3 {
+                    if *unsolved == expected {
+                        // it got solved - we can early return and not include this in result
+                        return false;
+                    }
+                    // check all rotations
+                    expected.rotate();
+                }
+                // all checks failed - not solved
+                return true;
+            }
+        );
+
+
+
+        /* let mut unsolved = Vec::with_capacity(self.unsolved.len());
         'l: for t in self.unsolved.iter_mut() {
             for _ in 0..3 {
                 if *t == self.expected {
@@ -48,6 +96,26 @@ impl<T: Clone + PartialEq> Solver<T> {
             }
             unsolved.push(t.clone())
         }
-        self.unsolved = unsolved;
+        self.unsolved = unsolved; */
+    }
+}
+
+impl<T: PartialEq> Solver<T> {
+    fn resolve_noclone(&mut self) {
+        self
+            .unsolved
+            .retain(|unsolved| {
+                for  _ in 0..3 {
+                    if *unsolved == self.expected {
+                        // it got solved - we can early return and not include this in result
+                        return false;
+                    }
+                    // check all rotations
+                    self.expected.rotate();
+                }
+                // all checks failed - not solved
+                return true;
+            }
+        );
     }
 }
