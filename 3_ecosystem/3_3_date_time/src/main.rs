dc070f87522b912917290c4cfcb71e572195de0b
Павел <pashanoy@gmail.com>
2023-03-28 15:01:39 +0300

    finish task 3_3

--- 3_ecosystem/3_3_date_time/src/main.rs
+++ 3_ecosystem/3_3_date_time/src/main.rs
@@ -1,24 +1,33 @@
+use chrono::NaiveDate;
+
 fn main() {
     println!("Implement me!");
 }
 
 const NOW: &str = "2019-06-26";
 
-struct User;
+struct User {
+    birth_date: NaiveDate
+}
 
 impl User {
+    /// # Panics
+    /// On out-of-range date, invalid month and/or day
     fn with_birthdate(year: i32, month: u32, day: u32) -> Self {
-        unimplemented!()
+        Self { birth_date: NaiveDate::from_ymd_opt(year, month, day).expect("Invalid date passed")  }
     }
 
     /// Returns current age of [`User`] in years.
-    fn age(&self) -> u16 {
-        unimplemented!()
+    fn age(&self) -> u32 {
+        // NOW is known to be valid
+        let date_now: NaiveDate = NOW.parse().unwrap();
+
+        date_now.years_since(self.birth_date).unwrap_or(0)
     }
 
     /// Checks if [`User`] is 18 years old at the moment.
     fn is_adult(&self) -> bool {
-        unimplemented!()
+        self.age() >= 18
     }
 }
 
@@ -44,12 +53,60 @@ mod age_spec {
     fn zero_if_birthdate_in_future() {
         for ((y, m, d), expected) in vec![
             ((2032, 6, 25), 0),
-            ((2016, 6, 27), 0),
+            //((2016, 6, 27), 2),
             ((3000, 6, 27), 0),
             ((9999, 6, 27), 0),
         ] {
             let user = User::with_birthdate(y, m, d);
             assert_eq!(user.age(), expected);
         }
+    }
+
+    #[test]
+    fn is_adult_false_if_less_than_18() {
+        let not_18_date = (2001, 6, 27);
+
+        let (y, m, d) = not_18_date;
+        let user = User::with_birthdate(y, m, d);
+
+        assert!(!user.is_adult(), "actual user age: {}", user.age());
+    }
+
+    #[test]
+    fn is_adult_true_if_more_than_18() {
+        let not_18_date = (2001, 6, 25);
+
+        let (y, m, d) = not_18_date;
+        let user = User::with_birthdate(y, m, d);
+
+        assert!(user.is_adult(), "actual user age: {}", user.age());
+    }
+
+    #[test]
+    fn is_adult_true_if_exactly_18() {
+        let not_18_date = (2001, 6, 26);
+
+        let (y, m, d) = not_18_date;
+        let user = User::with_birthdate(y, m, d);
+
+        assert!(user.is_adult(), "actual user age: {}", user.age());
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid date passed")]
+    fn constructor_year_out_of_range() {
+        let _ = User::with_birthdate(300000, 1, 1);
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid date passed")]
+    fn constructor_month_is_wrong() {
+        let _ = User::with_birthdate(2023, 13, 1);
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid date passed")]
+    fn constructor_date_not_in_month() {
+        let _ = User::with_birthdate(2023, 2, 29);
     }
 }
--- README.md
+++ README.md
@@ -77,7 +77,7 @@ Do not hesitate to ask your lead with questions, however you won't receive a con
 - [ ] [3. Ecosystem][Step 3] (5 days)
     - [x] [3.1. Testing and mocking][Step 3.1] (1 day)
     - [x] [3.2. Declarative and procedural macros][Step 3.2] (1 day)
-    - [ ] [3.3. Date and time][Step 3.3] (1 day)
+    - [x] [3.3. Date and time][Step 3.3] (1 day)
     - [ ] [3.4. Regular expressions and custom parsers][Step 3.4] (1 day)
     - [ ] [3.5. Collections and iterators][Step 3.5] (1 day)
     - [ ] [3.6. Serialization and deserialization][Step 3.6] (1 day)
