7c8ac04b6a9b845511a8757413420d26524ab4c7
Павел <pashanoy@gmail.com>
2023-04-04 12:38:26 +0300

    finish task 3_9

--- 3_ecosystem/3_9_cmd_env_conf/src/main.rs
+++ 3_ecosystem/3_9_cmd_env_conf/src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Implement me!");
+mod config_structs;
+
+use std::path::PathBuf;
+
+use clap::Parser;
+use config::{ConfigError, Config, File, Environment};
+use config_structs::MyConfig;
+use serde::Deserialize;
+
+#[derive(Parser, Debug)]
+#[command(version)]
+struct Args {
+    /// Enables debug mode
+    #[arg(short, long)]
+    debug: bool,
+
+    /// Path to configuration file [env: CONF_FILE=]
+    #[arg(short, long, value_name = "conf", default_value = "config.toml")]
+    config: PathBuf,
+}
+
+#[derive(Deserialize)]
+struct EnvConf {
+    conf_file: PathBuf
+}
+
+fn main() -> Result<(), ConfigError> {
+    let mut args = Args::parse();
+
+    if let Ok(env_conf) = envy::from_env::<EnvConf>() {
+        args.config = env_conf.conf_file;
+    }
+
+    let config = Config::builder()
+        .add_source(MyConfig::default())
+        .add_source(File::with_name(&args.config.to_string_lossy()))
+        .add_source(
+            Environment::with_prefix("CONF")
+        )
+        .build()?;
+
+    let res_config: MyConfig = config.try_deserialize().expect("Config to deserialize");
+    println!("{:?}", res_config);
+
+    Ok(())
 }
